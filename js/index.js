7208f495bde8082f9d0a00b7832e0aa97cae79b6
Jaehun Park <pjaehun4122@gmail.com>
2021-08-31 13:03:41 -0700

    added 3 event listner

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+// 10 different types of event listner
+
+// busimage onclick change to different picture
+const busImg=document.querySelector('.intro img');
+busImg.onclick= function(){
+    console.log('clicked on busimage');
+}
+
+// nav a mouseover change font color
+const navA=document.querySelectorAll('.nav a');
+navA.forEach(function(item){
+    item.addEventListener('mouseover',function(event){
+        console.log('about to press one of the nav a');
+        item.style.color="red";
+    })
+})
+
+// scroll will change text of nav a to bold
+const headOne=document.querySelector('*');
+headOne.addEventListener('wheel',function(event){
+    console.log("user is scrolling");
+   headOne.style.fontWeight="bold";
+
+})
